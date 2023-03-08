7ed3f7ec408a5a52904f7c21c7076e418a4c3e5f
ven <774777579@qq.com>
2023-03-09 00:55:22 +0800

    简单工厂模式

---
+++ 简单工厂模式.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+// 基础类
+type Person interface {
+	Work()
+}
+
+// 老师继承了基础类并实现了work方法
+type Teacher struct {
+	Person
+}
+
+func (t *Teacher) Work() {
+	fmt.Println("教书")
+}
+
+// 司机继承了基础类并实现了work方法
+type Driver struct {
+	Person
+}
+
+func (d *Driver) Work() {
+	fmt.Println("开车")
+}
+
+// 生产类
+type Factory struct {
+}
+
+func (f *Factory) CreatePerson(kind string) Person {
+	var p Person
+	if kind == "teacher" {
+		p = new(Teacher)
+	} else if kind == "driver" {
+		p = new(Driver)
+	}
+	return p
+}
+
+// 业务逻辑层
+func main() {
+	//创建一个工厂类
+	f := new(Factory)
+
+	// 创建一个老师
+	t := f.CreatePerson("teacher")
+	t.Work()
+
+	// 创建一个司机
+	d := f.CreatePerson("driver")
+	d.Work()
+}
