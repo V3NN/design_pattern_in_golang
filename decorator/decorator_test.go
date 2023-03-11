38b5ba0d28f7b54300c024438b0795c83384f609
ven <774777579@qq.com>
2023-03-11 22:08:27 +0800

    装饰器模式

---
+++ decorator/decorator_test.go
@@ -0,0 +1,19 @@
+package decorator
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDecorator(t *testing.T) {
+	huawei := new(HuaWei)
+
+	var keHuawei Phone
+	keHuawei = NewKeDecorator(huawei) //通过HueWei ---> KeHuaWei
+	keHuawei.Show()
+
+	fmt.Println("--------------------------")
+	var keMoHuaWei Phone
+	keMoHuaWei = NewMoDecorator(keHuawei) //通过KeHuaWei ---> KeMoHuaWei
+	keMoHuaWei.Show()
+}
